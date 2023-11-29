206044ff2300ebdea240533479ccb5b2e6b858df
Yogesh Rathore <yogeshrathore251@gmail.com>
2023-11-29 21:35:53 +0530

    Yogesh: Calculator FrameWork Added and Test File added

---
+++ Calculator-FrameWork/Calculator-FrameWork/Calculator.swift
@@ -0,0 +1,20 @@
+//
+//  Calculator.swift
+//  Calculator-FrameWork
+//
+//  Created by Yogesh Rathore on 29/11/23.
+//
+
+import Foundation
+
+public class Calculator {
+    
+    public init() {
+        
+    }
+    
+    public func multiplication(_ valueA: Int, with valueB: Int) -> Int {
+        
+        return valueA * valueB
+    }
+}
---
+++ Calculator-FrameWork/Calculator-FrameWork/Calculator_FrameWork.h
@@ -0,0 +1,18 @@
+//
+//  Calculator_FrameWork.h
+//  Calculator-FrameWork
+//
+//  Created by Yogesh Rathore on 29/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Calculator_FrameWork.
+FOUNDATION_EXPORT double Calculator_FrameWorkVersionNumber;
+
+//! Project version string for Calculator_FrameWork.
+FOUNDATION_EXPORT const unsigned char Calculator_FrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calculator_FrameWork/PublicHeader.h>
+
+
